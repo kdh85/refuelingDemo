03643c9d717ab3e4b297e5decc08e11abd93eee3
kdh85 <xerodin85@gmail.com>
2022-09-23 14:50:48 +0900

    Initial commit

---
+++ src/main/java/com/example/refuelingdemo/RefuelingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.refuelingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefuelingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefuelingdemoApplication.class, args);
+	}
+
+}
