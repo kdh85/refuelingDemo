23beef816c71210a3609dead44819204e8739f9b
kdh85 <xerodin85@gmail.com>
2022-09-23 15:23:58 +0900

    feat : init application java

--- src/main/java/com/example/refuelingdemo/RefuelingDemoApplication.java
+++ src/main/java/com/example/refuelingdemo/RefuelingDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RefuelingdemoApplication {
+public class RefuelingDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RefuelingdemoApplication.class, args);
+		SpringApplication.run(RefuelingDemoApplication.class, args);
 	}
 
 }
