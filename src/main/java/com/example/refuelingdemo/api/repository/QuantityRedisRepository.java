59078e8f23ab47caba513e92e8abda85dde1a0d1
kdh85 <xerodin85@gmail.com>
2022-09-27 12:53:29 +0900

    delete : 기존 샘플코드 제거.  - 개선된 방식의 redis 샘플 작성에 의한 기존 코드 폐기.

--- src/main/java/com/example/refuelingdemo/api/repository/QuantityRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.refuelingdemo.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.refuelingdemo.api.dto.Quantity;
-
-public interface QuantityRedisRepository extends CrudRepository<Quantity, String> {
-}
