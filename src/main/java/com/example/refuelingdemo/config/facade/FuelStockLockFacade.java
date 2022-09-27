1b1fd1bda254d4bfa2f84f8c7a76eccfe4c3937e
kdh85 <xerodin85@gmail.com>
2022-09-27 13:50:14 +0900

    feat : redis lock&unlock을 이용한 동시성 제어 facade 패턴 클래스 구현.  - sping lock을 사용하고 있기 때문에 동시성 이슈가 발생시 redis가 계속 lock을 체크함.  - 잦은 lock 체크가 redis 서버에 부하를 주기 때문에 주기를 완화하기 위해서 Thread sleep을 할당.(100ms. 실제 트래픽 테스트를 하여 최적화된 수치를 찾아야 함.)  - 재고 변경 후 unlock 처리.

---
+++ src/main/java/com/example/refuelingdemo/config/facade/FuelStockLockFacade.java
@@ -0,0 +1,34 @@
+package com.example.refuelingdemo.config.facade;
+
+import org.springframework.stereotype.Component;
+
+import com.example.refuelingdemo.api.enums.SleepTime;
+import com.example.refuelingdemo.api.repository.RedisRepository;
+import com.example.refuelingdemo.api.service.FuelService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FuelStockLockFacade {
+
+	private final RedisRepository redisRepository;
+
+	private final FuelService fuelService;
+
+	@SuppressWarnings("BusyWait")
+	public void decreaseFuelStock(final Long id, final Long useQuantity) throws InterruptedException {
+		log.info("### call FuelStockLockFacade id:{}, useQuantity:{}", id, useQuantity);
+		while (!redisRepository.isLock(id)) {
+			Thread.sleep(SleepTime.TIME_100.getMiles());
+		}
+
+		try {
+			fuelService.decreaseStock(id, useQuantity);
+		} finally {
+			redisRepository.unlock(id);
+		}
+	}
+}
