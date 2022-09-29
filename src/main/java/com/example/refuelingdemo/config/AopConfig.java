bdf112d43a900bb15f590c1836a9ecbe46c10a04
kdh85 <xerodin85@gmail.com>
2022-09-29 13:42:06 +0900

    feat : Aop config 구현.  - aop 기능을 사용하기 위한 config 설정 추가.

---
+++ src/main/java/com/example/refuelingdemo/config/AopConfig.java
@@ -0,0 +1,12 @@
+package com.example.refuelingdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@EnableAspectJAutoProxy
+@Configuration
+@Slf4j
+public class AopConfig {
+}
