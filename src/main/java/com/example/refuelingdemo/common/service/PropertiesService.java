1d4a9b8add088b0697dbba281fecc45a4a035c00
kdh85 <xerodin85@gmail.com>
2022-10-04 15:25:46 +0900

    feat : 설정값 생성 및 조회 기능 구현.  - 부모 설정값 생성 기능 추가.  - 부모값을 기준으로 자식 설정값 생성 기능 추가.  - 부모값이 없는 경우 에러 반환.  - 부모하나에 대한 N개의 자식 설정값 생성 기능 추가.  - 설정명 조회로 설정값 조회 기능 추가.(1:1)  - 부모설정 id로 자식 설정값들 조회 기능 추가.(1:N)

---
+++ src/main/java/com/example/refuelingdemo/common/service/PropertiesService.java
@@ -0,0 +1,49 @@
+package com.example.refuelingdemo.common.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.refuelingdemo.common.domain.Properties;
+import com.example.refuelingdemo.common.repository.PropertiesRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PropertiesService {
+
+	private final PropertiesRepository propertiesRepository;
+
+	@Transactional
+	public Properties createParentProperties(final String description, final String settingValue){
+		return propertiesRepository.save(
+			Properties.createParentProperties(description, settingValue)
+		);
+	}
+
+	@Transactional
+	public void createChildProperties(final String description, final String settingValue, Properties parent){
+		propertiesRepository.save(
+			Properties.createChildProperties(description, settingValue, parent)
+		);
+	}
+
+	@Transactional
+	public void createBulkChildProperties(List<Properties> children){
+		propertiesRepository.saveAll(children);
+	}
+
+	@Transactional(readOnly = true)
+	public Properties findByDescription(final String description){
+		return propertiesRepository.findByDescription(description);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Properties> findByParentId(final Long parentId){
+		return propertiesRepository.findAllByParentId(parentId);
+	}
+}
---
+++ src/test/java/com/example/refuelingdemo/common/service/PropertiesServiceTest.java
@@ -0,0 +1,126 @@
+package com.example.refuelingdemo.common.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.refuelingdemo.common.domain.Properties;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class PropertiesServiceTest {
+
+	@Autowired
+	private PropertiesService propertiesService;
+
+	@DisplayName("부모 설정값 생성 테스트.")
+	@Test
+	@Transactional
+	void createParentTest() {
+		String parentDesc = "latency";
+		String parentSetValue = "latencyValue";
+
+		Properties parentProperties = propertiesService.createParentProperties(parentDesc, parentSetValue);
+
+		assertAll(
+			() -> assertEquals(parentProperties.getDescription(), parentDesc),
+			() -> assertEquals(parentProperties.getSettingValue(), parentSetValue)
+		);
+	}
+
+	@DisplayName("부모 및 자식 설정값 생성 테스트.")
+	@Test
+	@Transactional
+	void createParentAndChildTest() {
+		//given
+		String parentDesc = "latency";
+		String parentSetValue = "latencyValue";
+		String childDesc = "sleep_3000";
+		String childSetValue = "3000";
+
+		//when
+		Properties parentProperties = propertiesService.createParentProperties(parentDesc, parentSetValue);
+		propertiesService.createChildProperties(childDesc, childSetValue, parentProperties);
+
+		//then
+		Properties findChild = propertiesService.findByDescription(childDesc);
+		log.info("findChild :{}", findChild);
+
+		assertAll(
+			() -> assertEquals(findChild.getParent().getDescription(), parentDesc),
+			() -> assertEquals(findChild.getParent().getSettingValue(), parentSetValue),
+			() -> assertEquals(findChild.getDescription(), childDesc),
+			() -> assertEquals(findChild.getSettingValue(), childSetValue),
+			() -> assertEquals(findChild.getSettingValueByLong(), Long.parseLong(childSetValue))
+		);
+	}
+
+	@DisplayName("부모없이 자식 설정값 생성시 에러 반환 테스트.")
+	@Test
+	@Transactional
+	void createChildFailTest() {
+		//given
+		String childDesc = "sleep_3000";
+		String childSetValue = "3000";
+
+		//then
+		assertThatThrownBy(
+			() -> propertiesService.createChildProperties(childDesc, childSetValue, null)
+		).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("N개의 자식 설정값 등록 테스트.")
+	@Test
+	void createChildBulkSTest() {
+		//given
+		String parentDesc = "latency";
+		String parentSetValue = "latencyValue";
+		String childDesc = "sleep";
+		List<String> childrenSetValue = List.of("100", "150", "200", "3000");
+
+		//when
+		Properties parentProperties = propertiesService.createParentProperties(parentDesc, parentSetValue);
+
+		List<Properties> children = childrenSetValue.stream()
+			.map(val -> Properties.createChildProperties(childDesc, val, parentProperties))
+			.collect(Collectors.toList());
+
+		log.info("children :{}", children);
+
+		propertiesService.createBulkChildProperties(children);
+
+		//then
+		List<Properties> findChild = propertiesService.findByParentId(parentProperties.getId());
+		log.info("findChild :{}", findChild);
+
+		List<String> allDesc = children.stream()
+			.map(Properties::getDescription)
+			.collect(Collectors.toList());
+
+		List<String> allSetValue = children.stream()
+			.map(Properties::getSettingValue)
+			.collect(Collectors.toList());
+
+		List<Long> allSetValueAsLong = children.stream()
+			.map(Properties::getSettingValueByLong)
+			.collect(Collectors.toList());
+
+		for (Properties properties : findChild) {
+			assertAll(
+				() -> assertThat(allDesc).contains(properties.getDescription()),
+				() -> assertThat(allSetValue).contains(properties.getSettingValue()),
+				() -> assertThat(allSetValueAsLong).contains(properties.getSettingValueByLong())
+			);
+		}
+	}
+}
