944964905d0c3ec25525215943aa5e9f07f81d88
kdh85 <xerodin85@gmail.com>
2022-10-04 15:23:10 +0900

    feat : 설정값 부모아이디로 조회 기능 추가.  - 네임드쿼리를 사용하여 부모id값으로 설정값 조회 기능 추가.

--- src/main/java/com/example/refuelingdemo/common/repository/PropertiesRepository.java
+++ src/main/java/com/example/refuelingdemo/common/repository/PropertiesRepository.java
@@ -1,10 +1,17 @@
 package com.example.refuelingdemo.common.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.refuelingdemo.common.domain.Properties;
 
 public interface PropertiesRepository extends JpaRepository<Properties, Long> {
 
 	Properties findByDescription(final String description);
+
+	@Query("select p from Properties p where p.parent.id= :parentId")
+	List<Properties> findAllByParentId(@Param("parentId") final Long parentId);
 }
