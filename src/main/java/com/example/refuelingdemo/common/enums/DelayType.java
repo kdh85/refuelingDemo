0e18ecaa69fb751a71087ae0eb13f382cf805a8f
kdh85 <xerodin85@gmail.com>
2022-10-05 13:50:26 +0900

    feat : delay 관련 타입 enum 정의.  - redis에서 사용되는 delay의 개념을 enum으로 정리.  - spin lock 타이밍에 대한 delay와 redis lock key값 생성시 지속시간을 정의함.  - 각 지연 타입별 기본할당되는 지연의 지속시간을 Sleep enum을 통해서 설정.(운영시 각 키별 지연시간 값을 db에서 변경해주는 방식 or enum을 추가/수정하여 사용.)  - 지연시간이라는 부모의 하위 계층으로 설정값이 생성됨에 따라 enum을 상속으로 사용해야하나 상속이 불가능한 관계로 interface를 통하여 확장이 가능하도록 구현.

---
+++ src/main/java/com/example/refuelingdemo/common/enums/DelayType.java
@@ -0,0 +1,25 @@
+package com.example.refuelingdemo.common.enums;
+
+import com.example.refuelingdemo.api.enums.SleepTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DelayType implements Type{
+	SPIN_LOCK_DELAY(SleepTime.TIME_250),
+	LOCK_DELAY(SleepTime.TIME_3000);
+
+	private final SleepTime sleepTime;
+
+	@Override
+	public DelayType[] getValues(){
+		return DelayType.values();
+	}
+
+	@Override
+	public String getValue() {
+		return this.name();
+	}
+}
