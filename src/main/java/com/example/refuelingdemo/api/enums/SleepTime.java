d5176c87dea77908d3200a9024f56e2f44afcf66
kdh85 <xerodin85@gmail.com>
2022-10-05 13:45:01 +0900

    feat : sleep 값 추가.  - 250ms에 해당하는 enum 필드 추가.  - 설정 entity에서 사용할 문자열 타입 필드값 메소드 구현.

--- src/main/java/com/example/refuelingdemo/api/enums/SleepTime.java
+++ src/main/java/com/example/refuelingdemo/api/enums/SleepTime.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum SleepTime {
-	TIME_3000(3000L), TIME_100(100L);
+	TIME_3000(3000L),
+	TIME_250(250L),
+	TIME_200(200L),
+	TIME_100(100L);
 
 	private final Long miles;
 
 	SleepTime(Long miles) {
 		this.miles = miles;
+	}
+
+	public String getMilesToString(){
+		return this.miles.toString();
 	}
 }
