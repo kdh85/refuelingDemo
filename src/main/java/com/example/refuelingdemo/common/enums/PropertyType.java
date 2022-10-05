ce6e854241b142cf23bc2e2affb8af49b177fb88
kdh85 <xerodin85@gmail.com>
2022-10-05 13:56:46 +0900

    feat, refactor : property type에 대한 interface 상속 적용 및 필드 추가.  - 기존에 1댑스로 관리하던 기준을 2뎁스로 변경함에 따라 세부 속성이 추가됨.(delayType 콜랙션 추가.)  - 2댑스 element 탐색 및 콜랙션 객체 반환 기능 추가.  - interface 메세지 기능 override 구현.

--- src/main/java/com/example/refuelingdemo/common/enums/PropertyType.java
+++ src/main/java/com/example/refuelingdemo/common/enums/PropertyType.java
@@ -1,14 +1,35 @@
 package com.example.refuelingdemo.common.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public enum PropertyType {
-	LATENCY("지연시간 설정값");
-	
+@AllArgsConstructor
+public enum PropertyType implements Type{
+	LATENCY("지연시간 설정값", Arrays.asList(DelayType.LOCK_DELAY, DelayType.SPIN_LOCK_DELAY));
+
 	private final String description;
 
-	PropertyType(String description) {
-		this.description = description;
+	private final List<DelayType> delayTypeList;
+
+	public static List<DelayType> findByPropertyType(PropertyType propertyType){
+		return Arrays.stream(PropertyType.values())
+			.filter(type -> type == propertyType)
+			.map(PropertyType::getDelayTypeList)
+			.findAny()
+			.orElseThrow(()-> new IllegalArgumentException("일치하는 PropertyType이 없습니다."));
+	}
+
+	@Override
+	public PropertyType[] getValues() {
+		return PropertyType.values();
+	}
+
+	@Override
+	public String getValue() {
+		return this.name();
 	}
 }
