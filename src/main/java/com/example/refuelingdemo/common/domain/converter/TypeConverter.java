4955720f88aad5e8f0b33714e8f8be20c36c7e88
kdh85 <xerodin85@gmail.com>
2022-10-05 14:36:11 +0900

    refactor : type converter 코드 개선.  - 기존에 List를 생성하고 각 enum들의 배열값을 addAll로 하여 콜랙션을 생성하여 값 탐색을 하는 구조를 stream 의 concat을 이용하여 개선.

--- src/main/java/com/example/refuelingdemo/common/domain/converter/TypeConverter.java
+++ src/main/java/com/example/refuelingdemo/common/domain/converter/TypeConverter.java
@@ -1,8 +1,7 @@
 package com.example.refuelingdemo.common.domain.converter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Stream;
 
 import javax.persistence.AttributeConverter;
 
@@ -23,19 +22,12 @@ public class TypeConverter implements AttributeConverter<Type, String> {
 
 	@Override
 	public Type convertToEntityAttribute(String dbData) {
-
-		List<Type> allPropertiesType = createAllPropertiesType();
-
-		return allPropertiesType.stream()
+		return Stream.concat(
+				Arrays.stream(PropertyType.values()),
+				Arrays.stream(DelayType.values())
+			)
 			.filter(type -> type.getValue().equalsIgnoreCase(dbData))
 			.findFirst()
 			.orElseThrow(() -> new IllegalArgumentException("일치하는 Type 이 없습니다."));
-	}
-
-	private static List<Type> createAllPropertiesType() {
-		List<Type> joinedType = new ArrayList<>();
-		joinedType.addAll(Arrays.asList(PropertyType.values()));
-		joinedType.addAll(Arrays.asList(DelayType.values()));
-		return joinedType;
 	}
 }
