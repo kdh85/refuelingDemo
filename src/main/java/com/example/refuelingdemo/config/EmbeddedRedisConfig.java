5294dc6430dd7437b339f15d116ab6d96fd25838
kdh85 <xerodin85@gmail.com>
2022-10-06 16:07:21 +0900

    chore : 내장 redis log 출력 추가.  - 시작, 종료에 대한 로그 출력 추가.(level : info)

--- src/main/java/com/example/refuelingdemo/config/EmbeddedRedisConfig.java
+++ src/main/java/com/example/refuelingdemo/config/EmbeddedRedisConfig.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
 
 @Profile("local")
 @Configuration
+@Slf4j
 public class EmbeddedRedisConfig {
 
 	@Value("${spring.redis.port}")
@@ -22,11 +24,13 @@ public class EmbeddedRedisConfig {
 	public void redisServerStart() {
 		redisServer = new RedisServer(port);
 		redisServer.start();
+		log.info("#### embedded redis start  port :{} #######",port);
 	}
 
 	@PreDestroy
 	private void redisServerStop() {
 		redisServer.stop();
+		log.info("#### embedded redis stop  port :{} #######",port);
 	}
 
 }
